583f7606d9f8b469d07093f8f8b5d1df2cd4cb2a
dbarjs <joseeduardobarros@gmail.com>
2022-09-10 16:46:01 -0300

    Create sticker-album page

---
+++ packages/app/pages/sticker-album.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { useHead } from "#app";
+import { getFirestore } from "@firebase/firestore";
+import { useBirthdays } from "@life-utils/birthdays";
+import { getFirebaseApp } from "@life-utils/shared";
+
+// IMPLEMENT
+getFirebaseApp();
+getFirebaseApp();
+getFirebaseApp();
+const database = getFirestore(getFirebaseApp());
+
+const { list } = useBirthdays({
+  database,
+  user: {
+    uid: "93pqGFyTnMNt4sZAcOym8VL0wFh2",
+  },
+});
+
+useHead({
+  title: "Birthdays",
+  titleTemplate: (title) => `${title} | Admin Panel`,
+});
+</script>
+
+<template>
+  <VList>
+    <VListItem v-for="item in list" :key="item.id" link>
+      <VListItemTitle>
+        <span>{{ item.displayName }}</span>
+      </VListItemTitle>
+    </VListItem>
+  </VList>
+</template>
