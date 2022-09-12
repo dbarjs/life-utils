50d1ef17bcabd831ed4d8e801887276b7bd99af2
dbarjs <joseeduardobarros@gmail.com>
2022-09-11 23:18:12 -0300

    Add support for info chips

--- packages/sticker-album/components/Sticker.vue
+++ packages/sticker-album/components/Sticker.vue
@@ -1,17 +1,38 @@
 <script lang="ts" setup>
 import { defineProps, defineEmits, PropType, computed, ref } from "vue-demi";
 import { IStickerAlbum } from "../types";
-import { VCard, VBtn, VIcon } from "vuetify/components";
+import { VCard, VBtn, VIcon, VChip } from "vuetify/components";
 
 const props = defineProps({
+  isLocalSelecting: { type: Boolean, required: true },
   sticker: { type: Object as PropType<IStickerAlbum.Sticker>, required: true },
   timesSelected: { type: Number, required: true },
+  timesSelectedOnFirestore: { type: Number, required: false },
 });
 const emit = defineEmits(["add-button:click", "remove-button:click"]);
 
-const isSelected = computed<boolean>(() => !!props.timesSelected);
+const selectCount = computed<number>(() => {
+  if (props.isLocalSelecting) {
+    return props.timesSelected;
+  }
+
+  return props.timesSelectedOnFirestore || 0;
+});
+const existsOnFirestore = computed<boolean>(
+  () => !!props.timesSelectedOnFirestore
+);
+const isSelected = computed<boolean>(() => {
+  if (props.isLocalSelecting) {
+    return !!props.timesSelected;
+  }
+
+  return !!props.timesSelectedOnFirestore;
+});
+const isDuplicated = computed<boolean>(() => props.timesSelected > 1);
 const classObject = computed(() => ({
-  "Sticker--isSelected": !!props.timesSelected,
+  "Sticker--existsOnFirestore": !!existsOnFirestore.value,
+  "Sticker--isSelected": !!isSelected.value,
+  "Sticker--isDuplicated": !!isDuplicated.value,
 }));
 const elevation = computed<number>(() => (!!props.timesSelected ? 3 : 1));
 
@@ -47,6 +68,30 @@ function onRemoveButtonClick() {
   >
     <div class="Sticker__main">
       <span class="Sticker__displayCode">{{ sticker.displayCode }}</span>
+
+      <div class="Sticker__infoWrapper">
+        <VChip
+          class="Sticker__infoChip Sticker__infoChip--isDuplicated"
+          v-if="isLocalSelecting && isSelected && existsOnFirestore"
+        >
+          <span>Repeated! :(</span>
+        </VChip>
+
+        <VChip class="Sticker__infoChip" v-else-if="existsOnFirestore">
+          <span>Already have! :-)</span>
+        </VChip>
+
+        <VChip
+          class="Sticker__infoChip Sticker__infoChip--isDuplicated"
+          v-else-if="isDuplicated"
+        >
+          <span>Ops! o/</span>
+        </VChip>
+
+        <VChip class="Sticker__infoChip" v-else-if="isSelected">
+          <span>One more! o/</span>
+        </VChip>
+      </div>
     </div>
 
     <div class="Sticker__actions">
@@ -59,7 +104,7 @@ function onRemoveButtonClick() {
         <VIcon>mdi-minus-circle-outline</VIcon>
       </VBtn>
 
-      <small class="Sticker__timesSelected">{{ timesSelected }}</small>
+      <small class="Sticker__timesSelected">{{ selectCount }}</small>
 
       <VBtn
         icon
@@ -74,25 +119,50 @@ function onRemoveButtonClick() {
 </template>
 
 <style lang="scss">
+// https://material.io/resources/color/#!/?view.left=0&view.right=0&secondary.color=3F51B5&primary.color=00838F&primary.text.color=ffffff&secondary.text.color=ffffff
 .Sticker {
   aspect-ratio: 10/13;
   align-items: center;
   background-color: #26c6da;
-  border-radius: 20px;
+  border-radius: 10px !important;
   display: flex;
   flex-direction: column;
   justify-content: center;
   user-select: none;
   text-align: center;
+  transition: background 10s ease;
   min-width: 7rem;
   max-width: 10rem;
+  margin: 2pt;
   width: 50%;
 
   &__main {
     align-items: center;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     flex: 1;
+    position: relative;
+    width: 100%;
+  }
+
+  &__infoWrapper {
+    left: 0;
+    position: absolute;
+    margin-top: 0.5rem;
+    top: 0;
+    width: 100%;
+  }
+
+  &__infoChip {
+    background-color: yellow;
+    border-radius: 20px !important;
+    font-size: 0.625rem !important;
+    font-weight: 700;
+
+    &--isDuplicated {
+      background-color: red;
+    }
   }
 
   &__displayCode {
@@ -122,6 +192,10 @@ function onRemoveButtonClick() {
 
   &--isSelected {
     background-color: #3f51b5;
+  }
+
+  &--isDuplicated {
+    background-color: #002984;
   }
 }
 </style>
